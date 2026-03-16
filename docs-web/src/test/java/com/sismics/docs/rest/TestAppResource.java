b0204691de53cd668826c07cbb5e9c8b517bcd13
infinityidk <andyzhoukaihang@gmail.com>
2026-03-16 06:21:34 +0000

    func "guest login" always on

--- docs-web/src/test/java/com/sismics/docs/rest/TestAppResource.java
+++ docs-web/src/test/java/com/sismics/docs/rest/TestAppResource.java
@@ -57,7 +57,7 @@ public class TestAppResource extends BaseJerseyTest {
         Long totalMemory = json.getJsonNumber("total_memory").longValue();
         Assert.assertTrue(totalMemory > 0 && totalMemory > freeMemory);
         Assert.assertEquals(0, json.getJsonNumber("queued_tasks").intValue());
-        Assert.assertFalse(json.getBoolean("guest_login"));
+        Assert.assertTrue(json.getBoolean("guest_login"));
         Assert.assertFalse(json.getBoolean("ocr_enabled"));
         Assert.assertEquals("eng", json.getString("default_language"));
         Assert.assertTrue(json.containsKey("global_storage_current"));
@@ -137,26 +137,11 @@ public class TestAppResource extends BaseJerseyTest {
      */
     @Test
     public void testGuestLogin() {
-        // Login admin
-        String adminToken = adminToken();
-
-        // Try to login as guest
-        Response response = target().path("/user/login").request()
-                .post(Entity.form(new Form()
-                        .param("username", "guest")));
-        Assert.assertEquals(Status.FORBIDDEN.getStatusCode(), response.getStatus());
-
-        // Enable guest login
-        target().path("/app/guest_login").request()
-                .cookie(TokenBasedSecurityFilter.COOKIE_NAME, adminToken)
-                .post(Entity.form(new Form()
-                        .param("enabled", "true")), JsonObject.class);
-
         // Login as guest
         String guestToken = clientUtil.login("guest", "", false);
 
         // Guest cannot delete himself
-        response = target().path("/user").request()
+        Response response = target().path("/user").request()
                 .cookie(TokenBasedSecurityFilter.COOKIE_NAME, guestToken)
                 .delete();
         Assert.assertEquals(Status.BAD_REQUEST.getStatusCode(), response.getStatus());
@@ -195,12 +180,6 @@ public class TestAppResource extends BaseJerseyTest {
         target().path("/document/list").request()
                 .cookie(TokenBasedSecurityFilter.COOKIE_NAME, guestToken)
                 .get(JsonObject.class);
-
-        // Disable guest login (clean up state)
-        target().path("/app/guest_login").request()
-                .cookie(TokenBasedSecurityFilter.COOKIE_NAME, adminToken)
-                .post(Entity.form(new Form()
-                        .param("enabled", "false")), JsonObject.class);
     }
 
     /**

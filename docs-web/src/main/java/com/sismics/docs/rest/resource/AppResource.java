d57b7854cfd063fc0ec853a826befee267473505
infinityidk <andyzhoukaihang@gmail.com>
2026-05-10 21:25:51 +0000

    update AppResource to disable OCR by default

--- docs-web/src/main/java/com/sismics/docs/rest/resource/AppResource.java
+++ docs-web/src/main/java/com/sismics/docs/rest/resource/AppResource.java
@@ -85,7 +85,7 @@ public class AppResource extends BaseResource {
         String currentVersion = configBundle.getString("api.current_version");
         String minVersion = configBundle.getString("api.min_version");
         Boolean guestLogin = true;
-        Boolean ocrEnabled = ConfigUtil.getConfigBooleanValue(ConfigType.OCR_ENABLED, true);
+        Boolean ocrEnabled = ConfigUtil.getConfigBooleanValue(ConfigType.OCR_ENABLED, false);
         String defaultLanguage = ConfigUtil.getConfigStringValue(ConfigType.DEFAULT_LANGUAGE);
         UserDao userDao = new UserDao();
         DocumentDao documentDao = new DocumentDao();
